032d6d7b3097c03e6aca18cf7f3348e0a8716703
Bryan Peguero <medranobryan22@gmail.com>
2020-10-28 16:57:33 -0400

    commit inicial

---
+++ appi.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI 
+import uvicorn
+import json
+import random
+
+#Inicializando la API
+app = FastAPI()
+
+#Ruta
+@app.get('/')
+def leer_ruta(name: str = "Default", lname: str = "Default", tl: int = "Default", country: str = "Default", sex: str = "Default"):
+    return {"id": random.randint(0, 1000),
+            "Name": name,
+            "Last Name": lname,
+            "Telephone": tl,
+            "Country": country,
+            "Genre": sex
+            }
+
+if __name__ == '_main_':
+    uvicorn.run(app, host='127.0.0.1', port='5500')
+
+
+
