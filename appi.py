25e6eb95098ff03c6fd64d144afc4658478fbd79
Bryan Peguero <medranobryan22@gmail.com>
2020-10-30 12:32:14 -0400

    second commit

--- appi.py
+++ appi.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-import json 
+import json
 import uvicorn
 import random
+
 
 #Init API
 app = FastAPI()
@@ -11,8 +12,20 @@ from products import products
 
 #Route
 @app.get('/products')
-def leer_ruta():
+def read_route():
     return {'Products Available': products}
+
+#Search a product
+@app.get('/products/{product_name}')
+def get_Product(product_name):
+    productFound = [product for product in products if product['name'] == product_name]
+    return {'The product was founded': productFound}
+
+#POST a product
+@app.post('/products/{product_name}')
+def post_Product():
+    products.append()
+    return {'Product added succesfully': products}
 
 if __name__ == '_main_':
     uvicorn.run(app, host="127.0.0.1",port='8000')
--- products.py
+++ products.py
@@ -1,19 +1,19 @@
 products = [
     {
         'name': 'PS5 Digital Edition',
-        'quantity': 10,
+        'in_stock': 10,
         'description': 'The new PS5 Digital Edition is here.',
         'price': 400
     },
     {
         'name': 'PS5 BlueRay Edition',
-        'quantity': 5,
+        'in_stock': 5,
         'description': 'The new brand new PS5 BlueRay Edition is here.',
         'price': 400
     }, 
     {
         'name': 'Xbox X',
-        'quantity': 2,
+        'in_stock': 2,
         'description': 'The new Xbox X arrive today.',
         'price': 400
     }
