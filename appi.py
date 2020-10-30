7e86b95635604e8a4985f51e6271e802a4490756
Bryan Peguero <medranobryan22@gmail.com>
2020-10-30 11:41:18 -0400

    Initial commit

--- appi.py
+++ appi.py
@@ -1,24 +1,21 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+import json 
 import uvicorn
-import json
 import random
 
 #Init API
 app = FastAPI()
 
+#Import the file products
+from products import products
+
 #Route
-@app.get('/')
-def leer_ruta(name: str = "Default", lname: str = "Default", tl: int = "Default", country: str = "Default", sex: str = "Default"):
-    return {"id": random.randint(0, 1000),
-            "Name": name,
-            "Last Name": lname,
-            "Telephone": tl,
-            "Country": country,
-            "Genre": sex
-            }
+@app.get('/products')
+def leer_ruta():
+    return {'Products Available': products}
 
 if __name__ == '_main_':
-    uvicorn.run(app, host='127.0.0.1', port='5500')
+    uvicorn.run(app, host="127.0.0.1",port='8000')
 
 
 
---
+++ products.py
@@ -0,0 +1,20 @@
+products = [
+    {
+        'name': 'PS5 Digital Edition',
+        'quantity': 10,
+        'description': 'The new PS5 Digital Edition is here.',
+        'price': 400
+    },
+    {
+        'name': 'PS5 BlueRay Edition',
+        'quantity': 5,
+        'description': 'The new brand new PS5 BlueRay Edition is here.',
+        'price': 400
+    }, 
+    {
+        'name': 'Xbox X',
+        'quantity': 2,
+        'description': 'The new Xbox X arrive today.',
+        'price': 400
+    }
+]
