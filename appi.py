5502844ab986827fab56e80f7ced961331149ef1
Bryan Peguero <medranobryan22@gmail.com>
2020-10-31 17:04:26 -0400

    Final commit

--- appi.py
+++ appi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 import json
 import uvicorn
 import random
@@ -32,6 +33,15 @@ def get_Product(product_name):
 def post_Product(product: Product):
     products.append(product.dict())
     return {'Product added succesfully': products}
+
+#DELETE a product
+@app.delete('/products/{product_name}')
+def delete_Product(product_name):
+    productFound = [product for product in products if product['name'] == product_name]
+    if (len(productFound) > 0):
+        products.remove(productFound[0])
+        return {'message': 'Product deleted',}
+    return {'message': 'Not Found'}
 
 if __name__ == '_main_':
     uvicorn.run(app, host="127.0.0.1",port='8000')
